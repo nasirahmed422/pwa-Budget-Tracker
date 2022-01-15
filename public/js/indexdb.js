d90f9f8a59dfc654aff35d23daf0ad3d6f005a53
Nasir Ahmed <nahmed@utilant.com>
2022-01-15 12:08:21 -0500

    indexdb updates

--- public/js/indexdb.js
+++ public/js/indexdb.js
@@ -1,3 +1,68 @@
 let db;
 // First get a connection to IndexedDB database
 const request = indexedDB.open('budget-tracker', 1);
+
+request.onupgradeneeded = function (event) {
+    const db = event.target.result;
+    db.createObjectStore('new_transaction', { autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+    // Save reference to db in global variable when db is successfully created with its object store
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        uploadtransaction();
+    }
+};
+
+request.onerror = function (event) {
+    console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    const budgetObjectStore = transaction.objectStore('new_transaction');
+
+    // Add record
+    budgetObjectStore.add(record);
+}
+
+function uploadtransaction() {
+    // open a transaction on your db
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    const budgetObjectStore = transaction.objectStore('new_transaction');
+    const getAll = budgetObjectStore.getAll();
+
+    getAll.onsuccess = function () {
+        // Send to the api server if there was data in indexedDb's store
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    // Open one more transaction
+                    const transaction = db.transaction(['new_transaction'], 'readwrite');
+                    const budgetObjectStore = transaction.objectStore('new_transaction');
+                    budgetObjectStore.clear();
+
+                    alert('All saved transactions have been submitted!');
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    };
+}
+
+// This listens for the app to come back online
+window.addEventListener('online', uploadtransaction);
